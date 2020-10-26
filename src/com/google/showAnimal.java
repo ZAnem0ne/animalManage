b28b7ab1082ae03d6fdaf51686a5eb6ec3fbf6d3
ZAnem0ne <619183868@qq.com>
2020-10-26 20:39:20 +0800

    123

--- src/com/google/showAnimal.java
+++ src/com/google/showAnimal.java
@@ -1,13 +1,21 @@
 package com.google;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class showAnimal {
     public static void showAnimal(ArrayList<Animal> array){
-        for (int i = 0; i <array.size() ; i++) {
-            Animal animal = new Animal();
-            animal = array.get(i);
-            System.out.println(animal.toString());
+//        使用fori遍历输出集合
+//        for (int i = 0; i <array.size() ; i++) {
+//            Animal animal = new Animal();
+//            animal = array.get(i);
+//            System.out.println(animal.toString());
+//        }
+        //使用迭代器进行输出集合
+        Iterator iterator = array.iterator();
+        while (iterator.hasNext()){
+            Object value = iterator.next();
+            System.out.println(value);
         }
     }
 }
