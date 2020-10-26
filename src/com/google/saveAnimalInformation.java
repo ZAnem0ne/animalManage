51a5699cf34eac996e2bc944e8561bf62beefec4
ZAnem0ne <619183868@qq.com>
2020-10-26 20:45:46 +0800

    123

--- src/com/google/saveAnimalInformation.java
+++ src/com/google/saveAnimalInformation.java
@@ -1,4 +1,18 @@
 package com.google;
 
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class saveAnimalInformation {
+    public void saveAnimalInformation() throws IOException {
+        File file = new File("animalInformation.txt");
+        if (!file.exists()) {
+            file.createNewFile();
+        }
+        FileUtils.writeLines(file,new ArrayList(Collections.singleton("春风很柔，夏风很烈，秋风清爽，东风凛冽，你就是春夏秋冬的风")), true);
+    }
 }
