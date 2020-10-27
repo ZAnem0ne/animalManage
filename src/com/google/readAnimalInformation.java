3b66fc38df77ac461d0ae1781c636e650b92efeb
ZAnem0ne <619183868@qq.com>
2020-10-27 23:07:31 +0800

    123

--- src/com/google/readAnimalInformation.java
+++ src/com/google/readAnimalInformation.java
@@ -6,14 +6,41 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class readAnimalInformation {
-    public static void readAnimalInformation(ArrayList<Animal> array) throws IOException{
+    public static void readAnimalInformation(ArrayList<Animal> array) throws IOException {
         BufferedReader br = new BufferedReader(new FileReader("animalInformation.txt"));
         //读取数据，每次读取一行数据
-        Animal line1 = new Animal();
-        String line2;
-        while((line2=br.readLine())!=null){
-            array.add(line1);
+        String line = br.readLine();
+        while (line != null) {
+            Animal animal = new Animal();
+            animal.setType(line);
+            line = br.readLine();
+            animal.setGender(line);
+            line = br.readLine();
+            animal.setAge(line);
+            line = br.readLine();
+            animal.setName(line);
+            line = br.readLine();
+            array.add(animal);
         }
+//            if (line.indexOf(";") > 0) {
+//                new Animal().setType(line);
+//                line = br.readLine();
+//                if (line.indexOf(";") > 0) {
+//                    new Animal().setGender(line);
+//                    line = br.readLine();
+//                    if (line.indexOf(";") > 0) {
+//                        new Animal().setAge(line);
+//                        line = br.readLine();
+//                        if (line.indexOf(";") > 0) {
+//                            new Animal().setName(line);
+//                            line = br.readLine();
+//                        }
+//                    }
+//                }
+//            }
+//        while((line2=br.readLine())!=null){
+//            array.add(line1);
+//        }
 
         //释放资源
         br.close();
